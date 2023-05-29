186f2b2eebd6660502ba4a3fc554497f3725f314
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-05-28 22:08:28 -0300

    commit3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './Imagenes/freecodecamp-logo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className= 'freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de freecodecamp' />
+      </div>
+      <div className='contenedor-calculadora'>
+        
+      </div>
     </div>
   );
 }
