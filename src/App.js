3aeb0afbb29742e1ec0e32a8cf1334f1d31dc9dc
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-05-30 21:22:37 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ const [input , setInput ] = useState('')
 
 const agregarInput = val => {
   setInput(input + val);
-}
+};
 
 const calcularResultado = () =>{
   if(input){setInput(evaluate(input));
   }else{
     alert('Por favor ingrese valores para realizar los calculos.')
-  }
+  };
   
 }
 
